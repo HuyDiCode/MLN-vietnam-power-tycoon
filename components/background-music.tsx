af0f22d087fcc3ce7abb08afb18a01c26915fde0
Huỳnh Nguyên <140372018+nguyenhcp2004@users.noreply.github.com>
2025-10-31 22:58:40 +0700

    feaqưeqweqwe

--- components/background-music.tsx
+++ components/background-music.tsx
@@ -7,6 +7,7 @@ export default function BackgroundMusic() {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
+  console.log("hehehehe");
 
   useEffect(() => {
     if (audioRef.current) {
